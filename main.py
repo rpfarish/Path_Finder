b999dd3c3cbb69f41d18094664d18ff4d49137a2
rpfarish <60075390+rpfarish@users.noreply.github.com>
2020-06-17 21:28:53 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,16 @@
 maze1 = [['|', '-', '-', '-', '-', '-', '|'],  # 0
          ['|', ' ', ' ', ' ', '+', ' ', '|'],  # 1
          ['|', ' ', ' ', ' ', '+', ' ', '|'],  # 2
-         ['|', ' ', '+', ' ', '+', ' ', '|'],  # 3
+         ['|', ' ', '+', '+', '+', ' ', '|'],  # 3
+         ['|', ' ', '+', '+', '+', ' ', '|'],  # 3
+         ['|', ' ', '+', '+', '+', ' ', '|'],  # 3
+['|', ' ', '+', '+', '+', ' ', '|'],  # 3
+['|', ' ', '+', '+', '+', ' ', '|'],  # 3
+['|', ' ', '+', '+', '+', ' ', '|'],  # 3
+['|', ' ', '+', '+', '+', ' ', '|'],  # 3
+         ['|', ' ', '+', '+', '+', ' ', '|'],  # 3
+         ['|', ' ', '+', '+', '+', ' ', '|'],  # 3
+         ['|', ' ', '+', '+', '+', ' ', '|'],  # 3
          ['|', ' ', ' ', ' ', ' ', 'X', '|'],  # 4
          ['|', '-', '-', '-', '-', '-', '|']]  # 5
 
@@ -65,7 +74,6 @@ def get_node_pos(path, node_col, node_row):
 
 def visualize_path(maze, path):
     for p in path:
-
         if p is True:
             pass
         else:
@@ -81,9 +89,9 @@ def check_paths(path, maze, node_col, node_row):
     visited_nodes = []
     path = [char for char in path]
     # this is a list of what the path was to the end node
-    
+
     for j in range(len(path)):
-        
+
         nd = path[j]
 
         if nd == "U":
@@ -127,57 +135,70 @@ def check_paths(path, maze, node_col, node_row):
 
 def all_paths(maze, node_col, node_row):
     """the maze param is only needed to pass to the valid move func"""
-    q = [""]
+    queue = [""]
     found = False
     while not found:
-        x = q[0]
-        q.pop(0)
+        # deque
+        e = queue[0]
+        queue.pop(0)
 
         # ------------------- U move ------------------------
-        cur_col, cur_row = get_node_pos(x, node_col, node_row)
+        cur_col, cur_row = get_node_pos(e, node_col, node_row)
         wall = is_wall("U", maze, cur_col, cur_row)
         if wall:
             pass
-        elif True:
-            up = x + "U"
-            q.append(up)
-            solution = check_paths(x, maze, node_col, node_row)
+        elif len(e) == 0:
+            up = e + "U"
+            queue.append(up)
+        elif e[-1] != "D":
+            up = e + "U"
+            queue.append(up)
+            solution = check_paths(e, maze, node_col, node_row)
             if solution:
                 return solution
 
         # ------------------ R move -------------------------
-        cur_col, cur_row = get_node_pos(x, node_col, node_row)
+        cur_col, cur_row = get_node_pos(e, node_col, node_row)
         wall = is_wall("R", maze, cur_col, cur_row)
         if wall:
             pass
-        elif True:
-            up = x + "R"
-            q.append(up)
-            solution = check_paths(x, maze, node_col, node_row)
+        elif len(e) == 0:
+            right = e + "R"
+            queue.append(right)
+        elif e[-1] != "L":
+            right = e + "R"
+            queue.append(right)
+            solution = check_paths(e, maze, node_col, node_row)
             if solution:
                 return solution
 
         # ----------------- D move --------------------------
-        cur_col, cur_row = get_node_pos(x, node_col, node_row)
+        cur_col, cur_row = get_node_pos(e, node_col, node_row)
         wall = is_wall("D", maze, cur_col, cur_row)
         if wall:
             pass
-        elif True:
-            up = x + "D"
-            q.append(up)
-            solution = check_paths(x, maze, node_col, node_row)
+        elif len(e) == 0:
+            down = e + "D"
+            queue.append(down)
+        elif e[-1] != "U":
+            down = e + "D"
+            queue.append(down)
+            solution = check_paths(e, maze, node_col, node_row)
             if solution:
                 return solution
 
         # ---------------- L move ---------------------------
-        cur_col, cur_row = get_node_pos(x, node_col, node_row)
+        cur_col, cur_row = get_node_pos(e, node_col, node_row)
         wall = is_wall("L", maze, cur_col, cur_row)
         if wall:
             pass
-        elif True:
-            up = x + "L"
-            q.append(up)
-            solution = check_paths(x, maze, node_col, node_row)
+        elif len(e) == 0:
+            left = e + "L"
+            queue.append(left)
+        elif e[-1] != "R":
+            left = e + "L"
+            queue.append(left)
+            solution = check_paths(e, maze, node_col, node_row)
             if solution:
                 return solution
 
